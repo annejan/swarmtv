5413d03f8dce45ef7df202eedf95035b2998be60
ranzbak <ranzbak@88541080-9ad6-de11-9093-5254007c1f44>
2010-01-13 07:59:49 +0000

    Fixed example filters.
    git-svn-id: https://old.setv.nl/svn/swarmtv@27 88541080-9ad6-de11-9093-5254007c1f44

--- trunk/rss-torrent/examples.sh
+++ trunk/rss-torrent/examples.sh
@@ -21,15 +21,17 @@ echo "Done."
 
 echo "Insert filters."
 ./rsstorrent -F "DollHouse:select link, title, pubdate, category, season, episode from newtorrents where title REGEXP('^[Dd]ollhouse') AND size < '400000000' AND new = 'Y'" \
-  -T "SELECT title FROM downloaded WHERE link=?1 OR (season>=?2 AND episode>?3 AND title REGEXP('^[Dd]ollhouse'))"
+  -T "SELECT title FROM downloaded WHERE link=?1 OR (season>=?2 AND episode>=?3 AND title REGEXP('^[Dd]ollhouse'))"
 ./rsstorrent -F "House:select link, title, pubdate, category, season, episode from newtorrents where title REGEXP('^[hH]ouse') AND size < '400000000' AND new = 'Y'" \
-  -T "SELECT title FROM downloaded WHERE link=?1 OR (season>=?2 AND episode>?3 AND title REGEXP('^[hH]ouse'))"
+  -T "SELECT title FROM downloaded WHERE link=?1 OR (season>=?2 AND episode>=?3 AND title REGEXP('^[hH]ouse'))"
 ./rsstorrent -F "Stargate Universe:select link, title, pubdate, category, season, episode from newtorrents where title REGEXP('^[sS]targate [Uu]niverse') AND size < '800000000' AND new = 'Y'" \
-  -T "SELECT title FROM downloaded WHERE link=?1 OR (season>=?2 AND episode>?3 AND title REGEXP('^[sS]targate [Uu]niverse'))"
+  -T "SELECT title FROM downloaded WHERE link=?1 OR (season>=?2 AND episode>=?3 AND title REGEXP('^[sS]targate [Uu]niverse'))"
 ./rsstorrent -F "BBC:select link, title, pubdate, category, season, episode from newtorrents where title REGEXP('^BBC') AND size < '800000000' AND new = 'Y'" \
   -T "SELECT title FROM downloaded WHERE link=?1"
 ./rsstorrent -F "Mythbusters:select link, title, pubdate, category, season, episode from newtorrents where title REGEXP('^[Mm]ythbusters') AND size < '800000000' AND new = 'Y'" \
-  -T "SELECT title FROM downloaded WHERE link=?1 OR (season>=?2 AND episode>?3 AND title REGEXP('^[mM]ythbusters'))"
+  -T "SELECT title FROM downloaded WHERE link=?1 OR (season>=?2 AND episode>=?3 AND title REGEXP('^[mM]ythbusters'))"
 ./rsstorrent -F "Dirty Jobs:select link, title, pubdate, category, season, episode from newtorrents where title REGEXP('^[dD]irty [jJ]obs') AND size < '400000000' AND new = 'Y'" \
-  -T "SELECT title FROM downloaded WHERE link=?1 OR (season>=?2 AND episode>?3 AND title REGEXP('^[dD]irty [jJ]obs'))"
+  -T "SELECT title FROM downloaded WHERE link=?1 OR (season>=?2 AND episode>=?3 AND title REGEXP('^[dD]irty [jJ]obs'))"
+./rsstorrent -F "Lost:select link, title, pubdate, category, season, episode from newtorrents where title REGEXP('^[Ll]ost') AND size < '400000000' AND new = 'Y'" \
+  -T "SELECT title FROM downloaded WHERE link=?1 OR (season>=?2 AND episode>=?3 AND title REGEXP('^[Ll]ost'))"
 echo "Done."
