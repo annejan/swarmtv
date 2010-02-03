1ec94ac666027dba7ca770a743fd0ed28b977638
ranzbak <ranzbak@88541080-9ad6-de11-9093-5254007c1f44>
2010-02-03 00:18:51 +0000

    Fixed parser cmake files.
    git-svn-id: https://old.setv.nl/svn/swarmtv@59 88541080-9ad6-de11-9093-5254007c1f44

--- trunk/rss-torrent/rssparser/twitter/plugin.cmake
+++ trunk/rss-torrent/rssparser/twitter/plugin.cmake
@@ -1,5 +1,5 @@
 #rsstorrentfilter
-SET(RSSTORRENT_PATH rssfilter/twitter)
+SET(RSSTORRENT_PATH rssparser/twitter)
 
 SET(SRC ${SRC} ${RSSTORRENT_PATH}/twitter ${RSSTORRENT_PATH}/twitparse ${RSSTORRENT_PATH}/splittext
 		${RSSTORRENT_PATH}/parsedate)
